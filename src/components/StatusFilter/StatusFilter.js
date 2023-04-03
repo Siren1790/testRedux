bbd93342670518078bebfa965548345a14b1bd06
Sergii Karpus <49605599+Siren1790@users.noreply.github.com>
2023-04-03 23:14:03 +0300

    Update

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,24 @@
 import { Button } from "components/Button/Button";
 import css from "./StatusFilter.module.css";
+// Імпортуємо хук
+import { useSelector, useDispatch } from "react-redux";
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "../../redux/constants";
+import { getStatusFilter } from "redux/selectors";
+import { setStatusFilter } from "redux/actions";
 
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
+  const dispatch = useDispatch();
+  // Викликаємо генератор екшену та передаємо значення фільтра
+  // Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all} onClick={() => handleFilterChange(statusFilters.all)}>All</Button>
+      <Button selected={filter === statusFilters.active} onClick={() => handleFilterChange(statusFilters.active)}>Active</Button>
+      <Button selected={filter === statusFilters.completed} onClick={() => handleFilterChange(statusFilters.completed)}>Completed</Button>
     </div>
   );
 };
